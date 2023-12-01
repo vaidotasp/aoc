9ff8bc24143f0dce63c3a4773f993296dea75e1a
Vaidotas Piekus <vaidotasp@gmail.com>
2023-12-01 12:15:42 -0500

    d1p2

--- 2023/d1/index.ts
+++ 2023/d1/index.ts
@@ -1,31 +1,38 @@
-const textInput = await Deno.readTextFile("./2023/d1/input2.txt");
+const textInput = await Deno.readTextFile("./2023/d1/input.txt");
 
 const lines = textInput.split("\n");
 
-// console.log(lines)
 
-const calibrationValues = [];
+const numberWords = {
+	one: '1',
+	two: '2',
+	three: '3',
+	four: '4',
+	five: '5',
+	six: '6',
+	seven: '7',
+	eight: '8',
+	nine: '9',
+};
 
+const digs = []
 for (const line of lines) {
-	console.log(line);
-	const lineWithNoLetters = line.split("").filter(c => Number(c));
-	if (lineWithNoLetters.length === 1) {
-		calibrationValues.push([lineWithNoLetters[0], lineWithNoLetters[0]])
-	}else {
-		calibrationValues.push([lineWithNoLetters[0], lineWithNoLetters[lineWithNoLetters.length -1]])
-	}
-	console.log(lineWithNoLetters)
+  const firstDigitText = line.match(/(\d|one|two|three|four|five|six|seven|eight|nine)/);
+  const lastDigitText = line.match(/.*(\d|one|two|three|four|five|six|seven|eight|nine).*/);
+  // console.log('lastDigitText', lastDigitText?.[1])
+  if (firstDigitText?.[0] && lastDigitText?.[1]) {
+    const dig1 = numberWords[firstDigitText?.[0]] || firstDigitText?.[0]
+    const dig2 = numberWords[lastDigitText?.[1]] || lastDigitText?.[1]
+    // console.log('yo', dig1,dig2)
+    digs.push([dig1, dig2]);
+  }
 }
 
-console.log(calibrationValues)
-console.log(calibrationValues.reduce((acc, val) => {
-	const num = Number(`${val[0]}${val[1]}`);
+const res = digs.reduce((acc, val) => {
+  const num = Number(`${val[0]}${val[1]}`);
 
-	const sum = Number(val[0]) + Number(val[1]);
 	acc = acc + num;
-	return acc
-}, 0))
+	return acc;
+}, 0);
 
-
-
-// console.log(textInput);
+console.log('calib', res);
